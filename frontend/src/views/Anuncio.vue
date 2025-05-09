1c47ce1eb26b81a5b52552a67f7df2577914650b
Ivirton <ivirton23@gmail.com>
2025-05-09 15:57:09 -0300

    comit

--- frontend/src/views/Anuncio.vue
+++ frontend/src/views/Anuncio.vue
@@ -2,56 +2,57 @@
 import Navegacao from '../components/Navegacao.vue'
 import AnuncioRotativo from '@/components/AnuncioRotativo.vue';
 import modalUploadAnuncio from '@/components/modalUploadAnuncio.vue';
+import axios from 'axios';
+import socket from '@/socket';
+import { onMounted, ref } from 'vue';
 
+const anuncios = ref([]);
 
+async function getAnuncio() {
+    try {
+        const response = await axios.get('/api/anuncios/');
+        if (response.data.erro) {
+            console.log('API não encontrada!');
+            return [];
+        } else {
+            console.log(response.data);
+            return response.data;
+        }
+    } catch (error) {
+        console.log('Erro ao buscar API:', error);
+        return [];
+    }
+}
 
-
+onMounted(async () => {
+    anuncios.value = await getAnuncio();
+});
 </script>
-<template>
 
+<template>
     <Navegacao />
 
     <main class="main">
-
         <div class="header">
-            
-            <div class="btn-group" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Novo</button>
-                <button type="button" class="btn btn-dark">Ativar</button>
-                <button type="button" class="btn btn-dark">Desativar</button>
-                <button type="button" class="btn btn-dark">Limpar</button>
-
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">Ações</button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                            data-bs-whatever="@mdo">Adicionar</a></li>
+                    <li><a class="dropdown-item" href="#">Ativar</a></li>
+                    <li><a class="dropdown-item" href="#">Desativar</a></li>
+                    <li><a class="dropdown-item text-danger" href="#">Remover todos</a></li>
+                </ul>
             </div>
-            <div class="linha">
-                
-            </div>
+            <div class="linha"></div>
         </div>
+
         <div class="anuncios">
-            <AnuncioRotativo imagemName="podium1.jpg" />
-            <AnuncioRotativo imagemName="podium1.jpg" />
-            <AnuncioRotativo imagemName="podium1.jpg" />
-            <AnuncioRotativo imagemName="podium1.jpg" />
-            <AnuncioRotativo imagemName="podium1.jpg" />
-            <AnuncioRotativo imagemName="podium1.jpg" />
-
-            <AnuncioRotativo imagemName="podium1.jpg" />
-
-            <AnuncioRotativo imagemName="podium1.jpg" />
-
-            <AnuncioRotativo imagemName="podium1.jpg" />
-
-            <AnuncioRotativo imagemName="podium1.jpg" />
-
-            <AnuncioRotativo imagemName="podium1.jpg" />
-
-            <AnuncioRotativo imagemName="podium1.jpg" />
-
-            <AnuncioRotativo imagemName="podium1.jpg" />
-
-
+            <AnuncioRotativo v-for="(anuncio, index) in anuncios" :key="index" :imagemName="anuncio.nome || 'default.jpg'" />
         </div>
-
     </main>
+
     <modalUploadAnuncio />
 </template>
 
@@ -62,7 +63,6 @@ import modalUploadAnuncio from '@/components/modalUploadAnuncio.vue';
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-
 }
 
 .h-100 {
@@ -80,6 +80,4 @@ import modalUploadAnuncio from '@/components/modalUploadAnuncio.vue';
 .main {
     /* width: 100%; */
 }
-
-
 </style>
