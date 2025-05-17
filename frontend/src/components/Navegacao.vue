b11bfa0b35ad090ec613f30d6b0dcb35dac25340
Ivirton <ivirton23@gmail.com>
2025-05-16 22:54:48 -0300

    ivirton nayron

--- frontend/src/components/Navegacao.vue
+++ frontend/src/components/Navegacao.vue
@@ -5,7 +5,7 @@ import { RouterLink, RouterView } from 'vue-router'
 <template> 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/">IDEV</a>
+            <a class="navbar-brand" href="/">IDEV - IVIRTON</a>
 
             <!-- <a class="navbar-brand" href="/<%=href%>"> <%= titulo %></a> -->
 
